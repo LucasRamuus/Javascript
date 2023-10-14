4fc262e4d16fc4331673ed6a7e4a966a949f0b6e
LucasRamuus <lucasgabrielpr12@gmail.com>
2023-10-14 18:45:03 -0300

    aula 9

---
+++ Fundamentos/aula09.js
@@ -0,0 +1,32 @@
+//pós incremento e pós decremento
+let num=10
+
+console.log(num++)//ele executa e depois incrementa o valor
+console.log(num--)//ele executa e depois decrementa o valor
+
+
+
+//pré incremento e pré decremento
+
+let num2=10
+
+console.log(++num)//ele incrimenta o valor e depois executa
+console.log(--num)//ele decrementa o valor e depois executa
+
+//operador de inversão
+
+let num3=10
+let x=-num3
+
+console.log(x)// com inversão ele deixou o valor negativo
+
+//concatenação
+
+let n1=10
+let n2=5
+
+console.log(n1+n2)
+console.log(n1+""+n2)//aqui acontece a concatenação
+
+
+
