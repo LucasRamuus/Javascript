928ffd8918833a4fdb575c97678d9b8652867255
LucasRamuus <lucasgabrielpr12@gmail.com>
2023-11-20 17:02:57 -0300

    aula 15

---
+++ Condicionais e Repetições/aula 15.js
@@ -0,0 +1,15 @@
+// Loop FOR em javascript
+for(let i=0; i < 10; i++){
+    console.log("Nome Aleatorio - valor do i: " + i)
+}
+
+
+// FOR com IF
+
+for(let i=0; i <10; i++){
+    if(i%2==0){
+        console.log(i + " e par")
+    }else{
+        console.log(i+ " e impar")
+    }
+}
