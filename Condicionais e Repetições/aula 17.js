e24dc63ebf398d0e22b5c87123b13e0722601365
LucasRamuus <lucasgabrielpr12@gmail.com>
2023-11-28 16:11:43 -0300

    aula 17

---
+++ Condicionais e Repetições/aula 17.js
@@ -0,0 +1,12 @@
+//Loop While 
+
+let n=5
+let fat=1
+//While acontece enquanto for verdadeiro e fica preso no loop
+while(n>=1){                  
+    fat*=n
+    //fat=fat*n
+    n--
+}
+
+console.log(fat)
